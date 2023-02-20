17d9b7b2db07ca83910889cf98db685730594699
YorlinPazos <111788223+YorlinPazos@users.noreply.github.com>
2023-02-19 19:20:09 -0500

    back on da house, ready for the front

--- api/src/controllers/name.js
+++ api/src/controllers/name.js
@@ -1,40 +1,40 @@
 const axios = require('axios');
 const { Recipe,Diet } = require('../db');
 const ModelCrud = require("./index"); 
-const { API_KEY } = process.env;
+const { API_KEY,API_KEY2 } = process.env;
 const { Op  } = require('sequelize')
 
 class RecipeModel extends ModelCrud{
     constructor(model){
         super(model)
     }
-
     getAll = async (req, res, next) =>{
-        if(req.query.name){                                       
+        try {
             const name = req.query.name;
-            try {
+            if(req.query.name){
                 let queryBdd = await this.model.findAll({
-                    attributes:['id', 'name', 'createdInDb'],
-                    where:{
-                        name : {
-                            [Op.iLike]: `%${name}%`  
-                        }, 
-                    },
-                    include: [{
-                        model: Diet,
-                        attributes: ['name'],   //incluyo el modelo Diet
-                }] 
-            })
+                  attributes:['id', 'name', 'createdInDb'],
+                  where:{
+                     name : {
+                             [Op.iLike]: `%${name}%`  
+                              }, 
+                        },
+                        include: [{
+                         model: Diet,
+                         attributes: ['name'],   //incluyo el modelo Diet
+                         }] 
+                })
                 let mapClearDB = queryBdd.map(el => {
-                    return{
-                        id: el.id,
-                        name: el.name,           //Limpio un poco los results
-                        diets: el.diets.map(el => el.name).join(', ')
-                    }
-                })                  
-                                        // API
+                return{
+                       id: el.id,
+                       name: el.name,           //Limpio un poco los results
+                       diets: el.diets.map(el => el.name).join(', ')
+                       }
+                    })    
+                    
+                                              //  API
 
-                let queryApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
+                let queryApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY2}&addRecipeInformation=true&number=100`)
                 let filterResult = queryApi.data.results.filter(el => el.title.toLowerCase().includes(name.toLowerCase()))
              
                     let mapClearApi = await filterResult.map(el => {
@@ -45,12 +45,23 @@ class RecipeModel extends ModelCrud{
                         }
                     })
                    let results = [...mapClearDB, ...mapClearApi]
-    
-                   res.status(200).send(results);
+                   if(results.length){
+                    res.status(200).send(results);
+                   }else{
+                    res.status(404).json({
+                        message: `No existe la receta con el nombre ${name}`
+                    })
+                   }
+
+            } else {
+            res.status(404).json({
+                message: 'Ingrese un nombre antes de continuar con la búsqueda'
+            }) 
+                }
             } catch (error) {
-                next(error)
-            }
+            next(error)
         }
+      
     }   
 }
 
@@ -58,3 +69,4 @@ class RecipeModel extends ModelCrud{
 const nameController = new RecipeModel(Recipe);
 
 module.exports = nameController;
+
--- api/src/controllers/recipes.js
+++ api/src/controllers/recipes.js
@@ -1,8 +1,8 @@
 const ModelCrud = require("./index");
 const axios = require('axios');
 const { Recipe, Diet } = require('../db');
-const { API_KEY } = process.env;
-const { Op  } = require('sequelize')
+const { API_KEY,API_KEY2 } = process.env;
+
 
 class RecipeModel extends ModelCrud{
     constructor(model){
@@ -43,7 +43,7 @@ class RecipeModel extends ModelCrud{
                 let obj = {};
                  let recipeIdApi;
         try {
-            recipeIdApi = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+            recipeIdApi = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY2}`);
             obj = {
                 name: recipeIdApi.data.title,
                 id: recipeIdApi.data.id,
@@ -62,51 +62,7 @@ class RecipeModel extends ModelCrud{
     next(error);
     }
 }   
-                                                //By Name
-
-    getAll = async (req, res, next) =>{
-        if(req.query.name){                                       
-            const name = req.query.name;
-            try {
-                let queryBdd = await this.model.findAll({
-                    attributes:['id', 'name', 'createdInDb'],
-                    where:{
-                        name : {
-                            [Op.iLike]: `%${name}%`  
-                        }, 
-                    },
-                    include: [{
-                        model: Diet,
-                        attributes: ['name'],   //incluyo el modelo Diet
-                }] 
-            })
-                let mapClearDB = queryBdd.map(el => {
-                    return{
-                        id: el.id,
-                        name: el.name,           //Limpio un poco los results
-                        diets: el.diets.map(el => el.name).join(', ')
-                    }
-                })                  
-                                        // API
-
-                let queryApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`)
-                let filterResult = queryApi.data.results.filter(el => el.title.toLowerCase().includes(name.toLowerCase()))
-             
-                    let mapClearApi = await filterResult.map(el => {
-                        return{
-                            id: el.id,
-                            name: el.title,
-                            diets: el.diets.map(el => el).join(', ')
-                        }
-                    })
-                   let results = [...mapClearDB, ...mapClearApi]
-    
-                   res.status(200).send(results);
-            } catch (error) {
-                next(error)
-            }
-        }
-    }   
+                  
                                                 //get All x100 recipes
     getAll = async (req, res, next) =>{
         try {
@@ -133,7 +89,7 @@ class RecipeModel extends ModelCrud{
             let results = [...mapGetAllClear]
 
 
-            let response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?&addRecipeInformation=true&number=100&apiKey=${API_KEY}`)
+            let response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?&addRecipeInformation=true&number=100&apiKey=${API_KEY2}`)
             let recipesMapApi = response.data.results.map(el =>{
                 return{
                     name: el.title,
